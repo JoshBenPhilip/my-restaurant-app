ef1405a844da6a8f314ffe8218222a41cc179289
JoshBenPhilip <joshben4c@gmail.com>
2022-05-26 14:58:18 -0400

    working app

---
+++ src/scenes/Details.js
@@ -0,0 +1,28 @@
+import { useContext } from "react"
+import { View, Text, Image, ActivityIndicator } from "react-native"
+import { SingleRestContext } from "../../App"
+import styles from "../styles"
+
+export default function Details() {
+    const {currentRest} = useContext(SingleRestContext)
+    return (
+        <View style= {styles.restaurantCard}>
+            {!currentRest
+            ? <ActivityIndicator/>
+            : (
+                <>
+                <Image source={{uri: currentRest.image}} 
+                style={{ width:'100%', height: 240}}/>
+                <Text style = {styles.restaurantsName}>{currentRest.name}</Text>
+                <Text style = {styles.cuisine}>{currentRest.cuisine}</Text>
+                <Text style = {styles.address}>{currentRest.address}</Text>
+                <Text style = {[styles.address, { fontWeight: '700'}]}>
+                    Rating: {currentRest.rating}</Text>
+                </>
+            )
+        }
+
+
+        </View>
+    )
+}
