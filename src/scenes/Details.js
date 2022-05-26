6c3ec75d302ceefee4136b72aacc227f335cd6c2
JoshBenPhilip <joshben4c@gmail.com>
2022-05-26 16:47:05 -0400

    nonfunctioning rating method added

--- src/scenes/Details.js
+++ src/scenes/Details.js
@@ -1,10 +1,17 @@
 import { useContext } from "react"
 import { View, Text, Image, ActivityIndicator } from "react-native"
+import { Button } from "react-native"
 import { SingleRestContext } from "../../App"
 import styles from "../styles"
 
 export default function Details() {
     const {currentRest} = useContext(SingleRestContext)
+    const handleRating = (newRating) => {
+        fetch()
+        .then()
+        .then()
+        .catch()
+    }
     return (
         <View style= {styles.restaurantCard}>
             {!currentRest
@@ -17,12 +24,19 @@ export default function Details() {
                 <Text style = {styles.cuisine}>{currentRest.cuisine}</Text>
                 <Text style = {styles.address}>{currentRest.address}</Text>
                 <Text style = {[styles.address, { fontWeight: '700'}]}>
-                    Rating: {currentRest.rating}</Text>
+                    Rating: {currentRest.rating}
+                </Text>
+                <View style ={{ flexDirection: 'row', justifyContent: 'space-evenly', paddingVertical: 24,
+                }}>
+                    <Button title="1" />
+                    <Button title="2" />
+                    <Button title="3" />
+                    <Button title="4" />
+                    <Button title="5" />
+                </View>
                 </>
             )
         }
-
-
         </View>
     )
 }
